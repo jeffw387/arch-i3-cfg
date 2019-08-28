8badf42ac17f9a231144c476578ab430545f6d01
Jeff Wright <jeff@teamjk.page>
2019-08-28 16:41:32 -0700

    add neovim to install list, include bash cfgs

--- theme-setup.sh
+++ theme-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -Ss i3 i3status i3lock git base-devel feh alacritty ranger gdm
+sudo pacman -Ss i3 i3status i3lock git base-devel feh alacritty ranger gdm neovim
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
