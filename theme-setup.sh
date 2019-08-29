2710cbbc83e2445426c567199ca040ad2cbc38e9
Jeff Wright <jeff@teamjk.page>
2019-08-28 23:41:04 -0700

    install not query, add rofi

--- theme-setup.sh
+++ theme-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -Ss i3 i3status i3lock git base-devel feh alacritty ranger gdm neovim
+sudo pacman -S i3 i3status i3lock git base-devel feh alacritty ranger gdm neovim rofi
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
